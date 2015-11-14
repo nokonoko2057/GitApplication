a925fb84829c51cf1ec3b47f81dd55d5a8e9f7b4
yuki takei <nokonoko2057@gmail.com>
2015-11-14 12:35:20 +0900

    second commit

--- GitApplication/ViewController.swift
+++ GitApplication/ViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var la: UILabel!
+    var num :Int = 0 
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +22,12 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func plus(sender: AnyObject) {
+        
+        num++
+        la.text = String(num)
+    }
 
 }
+
 
